31d2bb2219f95733f811dd713825a0bfa7bebbcf
yidui.yi@outlook.com <yidui.yi@outlook.com>
2025-09-29 23:45:25 +0800

    xu bao chi di san zai zhong dong di qu wo men de zhi neng shou ji chu huo liang shi zhan lv da 17

---
+++ assets/website/js/frontend/free_clash_meta/custom-swiper-1.js
@@ -0,0 +1,32 @@
+const swiper = new Swiper('.swiper', {
+
+  autoplay: {
+     delay: 3000,
+     disableOnInteraction: false
+   },
+
+  effect: 'slide', //"slide", "fade", "cube", "coverflow", "flip"
+
+  // Optional parameters
+  direction: 'horizontal',
+  slidesPerView: 1,
+  loop: true,
+  speed: 1200,
+  mousewheel: false,
+  watchSlidesProgress: true,
+  parallax: true,
+  spaceBetween: -1,
+
+  // If we need pagination
+   pagination: {
+      el: ".swiper-pagination",
+      type: "fraction",
+    },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+});
